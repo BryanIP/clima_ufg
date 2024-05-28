a6e6594fc4867e215f690fa59852dd767ced69ab
Bryan Rodrigues Bento <bryanrodriguesip@gmail.com>
2024-05-27 23:01:38 -0300

    [NEW] Container with photo

--- lib/pages/home/widgets/container_photo.dart
+++ lib/pages/home/widgets/container_photo.dart
@@ -15,6 +15,11 @@ class ContainerPhoto extends GetWidget {
       width: 300,
       decoration: BoxDecoration(
           color: defaultBlue.withOpacity(0.5),
+          image: DecorationImage(
+              image: Image.network(
+                      'https://www.qualviagem.com.br/wp-content/uploads/2015/12/pra%C3%A7a-goiania-goias.jpg')
+                  .image,
+              fit: BoxFit.cover),
           borderRadius: BorderRadius.circular(50.0)),
     );
   }
