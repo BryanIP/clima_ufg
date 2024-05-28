6c4e3219b7a4462e2ff9f3c53bde18f0f24c5665
Bryan Rodrigues Bento <bryanrodriguesip@gmail.com>
2024-05-28 00:22:24 -0300

    Ternary Operator and Controller

--- lib/pages/home/widgets/container_photo.dart
+++ lib/pages/home/widgets/container_photo.dart
@@ -1,15 +1,20 @@
 import 'package:clima_ufg/core/colors.dart';
+import 'package:clima_ufg/pages/home/controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ContainerPhoto extends GetWidget {
+class ContainerPhoto extends GetWidget<HomeController> {
   const ContainerPhoto({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(
+      () => HomeController(),
+    );
+
     return Container(
       margin: const EdgeInsets.all(10),
       height: 100,
@@ -30,13 +35,30 @@ class ContainerPhoto extends GetWidget {
             opacity: 0.9,
             child: Container(
               height: 60,
-              width: 110,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
+              width: 120,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
                       bottomRight: Radius.circular(50.0),
                       topLeft: Radius.circular(50.0)),
-                  gradient:
-                      LinearGradient(colors: [defaultBlue, defaultGreen])),
+                  gradient: LinearGradient(colors: [
+                    controller.temp < 30.0 ? defaultBlue : defaultRed,
+                    controller.temp < 30.0 ? defaultGreen : defaultYellow
+                  ])),
+              child: Center(
+                child: Obx(() => Text(
+                      '${controller.temp}°',
+                      style: const TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: defaultWhite,
+                          shadows: [
+                            Shadow(
+                                color: defaultBlack,
+                                blurRadius: 10.0,
+                                offset: Offset(2.0, 2.0))
+                          ]),
+                    )),
+              ),
             ),
           )
         ],
